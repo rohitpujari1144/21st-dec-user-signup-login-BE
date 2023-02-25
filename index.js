3ac4b588c1b81373682fb4c254f164bdd04eea09
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-02-25 17:53:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors=require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
-const dbUrl = 'mongodb://127.0.0.1:27017/'
+const dbUrl = 'mongodb://127.0.0.1:27017'
 const client = new MongoClient(dbUrl)
 
 
